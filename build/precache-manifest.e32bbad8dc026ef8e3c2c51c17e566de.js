d30f5f7ff983c284c60fd6b0c7d9905acd99268d
PritamMittal <pritammittal987@gmail.com>
2021-07-09 13:18:47 +0530

    notes-app-completed

---
+++ build/precache-manifest.e32bbad8dc026ef8e3c2c51c17e566de.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b4d285e89e7ddfb7e3c2ba350277258",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e069865251551a376e1",
+    "url": "/static/js/2.5a67b48e.chunk.js"
+  },
+  {
+    "revision": "293187e7988bfd71259e",
+    "url": "/static/js/main.0318ac1b.chunk.js"
+  },
+  {
+    "revision": "583e51a7281672351041",
+    "url": "/static/js/runtime-main.38f647cd.js"
+  }
+]);
